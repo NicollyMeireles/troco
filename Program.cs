1d0caade37809d5adc6a05a7ee8d04ebc7cf2886
NicollyMeireles <nicollymeireles32@gmail.com>
2023-03-31 22:38:13 -0300

    troco

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿decimal valorCompra, valorPago, valorTroco;
+Console.WriteLine(" valor do Troco \n");
+Console.Write("Valor da compra (R$)...: ");
+valorCompra = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Valor pago (R$)........: ");
+valorPago = Convert.ToDecimal(Console.ReadLine());
+valorTroco = valorPago - valorCompra;
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine($"\nTroco: {valorTroco:C}");
